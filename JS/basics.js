c7f41fafb1249cbebba4fba75d56d569b7455194
Alan Bedoya <alanjbedoya@gmail.com>
2021-01-09 23:58:39 -0800

    Adding to JS notes folder.

--- JS/basics.js
+++ JS/basics.js
@@ -23,6 +23,38 @@ console.log('Hi there!') //Prints: Hi there!
 
 
 Numbers
+Numbers are a primitive data type. They include the set of all integers and floating point numbers.
+Ex:
+let amount = 6; 
+let price = 4.99;
+
+
+String .length 
+The .length property of a string returns the number of characters that make up the string. 
+Ex:
+let message = 'good nite';
+console.log(message.length);
+// Prints: 9
+console.log('howdy'.length);
+// Prints: 5
+
+
+Data Instances
+When a new piece of data is introduced into a JavaScript program, the program keeps track of it in an instance of that data type. An instance is an individual case of a data type.
+
+
+Booleans
+Booleans are a primitive data type. They can be either true or false.
+Ex:
+let lateToWork = true;
+
+
+Math.random()
+The Math.random() function returns a floating-point, random number in the range from 0 (inclusive) up to but not including 1.
+Ex:
+console.log(Math.random());
+// Prints: 0 - 0.9
+
 
 
 
