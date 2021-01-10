030641e88d6075f8160616c24796516285d38286
Alan Bedoya <alanjbedoya@gmail.com>
2021-01-09 23:25:46 -0800

    Adding to JS notes folder.

---
+++ JS/basics.js
@@ -0,0 +1,28 @@
+/*
+JavaScript
+JavaScript is a programming language that powers the dynamic behavior on most websites. Alongside HTML and CSS, it is a core technology that makes the web run.
+
+
+Methods
+Methods return information about an object, and are called by appending an instance with a period, the mehtod name, and parentheses. 
+Ex:
+Math.random(); //returns a number between 0 and 1
+
+
+Libraries
+Libraries contain methods that can be called by appending the library name with a period ., the method name, and a set of parentheses.
+Ex:
+Math.random(); //Math is the library
+
+
+console.log()
+The console.log() method is used to log or print messages to the console. It can also be use to print objects and other info. 
+Ex:
+console.log('Hi there!') //Prints: Hi there!
+
+
+Numbers
+
+
+
+*/
