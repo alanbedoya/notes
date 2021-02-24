c245daabb1360be0bf812d7fb2fa06835f08dd47
Alan Bedoya <alanjbedoya@gmail.com>
2021-02-24 00:59:07 -0800

    Working on JS exercises 1.

--- JS: From Fundamentals to Functional JS, v2/exercises.js
+++ JS: From Fundamentals to Functional JS, v2/exercises.js
@@ -1,6 +1,9 @@
 //Exercise 1
 /* Create an object using bracket and dot notation that represents the characters and related data you may find in a game of Clue. */
 let game = {};
+game.name = [];
+game.name[0] = 'Miss Sarlet';
+
 game.murderer = '??';
 game['weapons'] = [
   { type: 'laser', location: 'lab' },
