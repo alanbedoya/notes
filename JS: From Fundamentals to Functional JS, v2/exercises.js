756fec4ed4fe496e7d613c6d3da3f66a8863e632
Alan Bedoya <alanjbedoya@gmail.com>
2021-02-24 00:08:27 -0800

    Working on JS exercises.

---
+++ JS: From Fundamentals to Functional JS, v2/exercises.js
@@ -0,0 +1,6 @@
+//Exercise 1
+/* Create an object using bracket and dot notation that represents the characters and related data you may find in a game of Clue. */
+let person = [];
+person.name = 'Mr. Bedoya';
+
+consoel.log(person);
