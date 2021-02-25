42dbdcbb541d88ddfc608c352a0529565c3bf577
Alan Bedoya <alanjbedoya@gmail.com>
2021-02-24 21:33:51 -0800

    Finished JS exercises 3-2, 3-3.

--- JS: From Fundamentals to Functional JS, v2/exercises.js
+++ JS: From Fundamentals to Functional JS, v2/exercises.js
@@ -35,25 +35,70 @@ const {first, last} = {first: 'Alan', last: 'Bedoya};
 // console.log(obj);
 
 //List Transforms
-//Exercise 3
+//Exercise 3-1
 //Loop through the suspects array
-const game = {
-  suspects: [
-    {
-      name: 'Alan',
-      color: 'Teal',
-    },
-    {
-      name: 'Emily',
-      color: 'Purple',
-    },
-  ],
-};
+// const game = {
+//   suspects: [
+//     {
+//       name: 'Alan',
+//       color: 'Teal',
+//     },
+//     {
+//       name: 'Emily',
+//       color: 'Purple',
+//     },
+//   ],
+// };
 
-function foo() {
-  for (let i = 0; i < game.suspects.length; i++) {
-    console.log(game.suspects[i]);
-  }
-}
+// function foo() {
+//   for (let i = 0; i < game.suspects.length; i++) {
+//     console.log(game.suspects[i]);
+//   }
+// }
 
-foo();
+// foo();
+
+//Exercise 3-2
+//Loop through all the properties of the suspect objects in the suspects array, mark them if you think they are guilty.
+// let game = {
+//   suspects: [
+//     {
+//       name: 'Rusty',
+//       color: 'orange',
+//     },
+//     {
+//       name: 'Miss Scarlet',
+//       color: 'red',
+//     },
+//   ],
+// };
+
+// let gameLoop = function () {
+//   for (let i = 0; i < game.suspects.length; i++) {
+//     for (let key in game.suspects[i]) {
+//       if (game.suspects[i][key] === 'Miss Scarlet') {
+//         console.log("Got 'em!");
+//       } else {
+//         console.log('Keep searching.');
+//       }
+//     }
+//   }
+// };
+
+// gameLoop();
+
+//Exercise 3-3
+//Destructure this nested data structure into two variables with the strings 'red' and 'orange'.
+var suspects = [
+  {
+    name: 'Rusty',
+    color: 'orange',
+  },
+  {
+    name: 'Miss Scarlet',
+    color: 'red',
+  },
+];
+
+let [color, color2] = [suspects[0].color, suspects[1].color];
+console.log(color, color2);
