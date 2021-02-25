8151f5310a7b3815df038cbab563ce0fc6b74c6f
Alan Bedoya <alanjbedoya@gmail.com>
2021-02-24 16:48:48 -0800

    Finished JS exercises 2.

--- JS: From Fundamentals to Functional JS, v2/exercises.js
+++ JS: From Fundamentals to Functional JS, v2/exercises.js
@@ -1,18 +1,18 @@
 //Exercise 1
 /* Create an object using bracket and dot notation that represents the characters and related data you may find in a game of Clue. */
-let game = {};
-game.name = [];
-game.name[0] = 'Miss Sarlet';
+// let game = {};
+// game.name = [];
+// game.name[0] = 'Miss Sarlet';
 
-game.murderer = '??';
-game['weapons'] = [
-  { type: 'laser', location: 'lab' },
-  { type: 'knife', location: 'kitchen' },
-  { type: 'angry cats', location: 'bedroom' },
-  { type: 'posion', location: 'kitchen' },
-];
+// game.murderer = '??';
+// game['weapons'] = [
+//   { type: 'laser', location: 'lab' },
+//   { type: 'knife', location: 'kitchen' },
+//   { type: 'angry cats', location: 'bedroom' },
+//   { type: 'posion', location: 'kitchen' },
+// ];
 
-console.log(game);
+// console.log(game);
 
 //ES6 Destructuring
 /*
@@ -23,8 +23,13 @@ var last = obj.last;
 const {first, last} = {first: 'Alan', last: 'Bedoya};
 */
 
-
 //Exercise 2 (Destructuring)
-{"name": "Rusty", "room":"kitchen", "weapon":"candlestick"};
+// {"name": "Rusty", "room":"kitchen", "weapon":"candlestick"};
 
-const {name, room, weapon} = {name: 'Rusty', room: 'kitchen', weapon: 'candlestick'};
+const obj = ({ person, room, weapon } = {
+  person: 'Rusty',
+  room: 'kitchen',
+  weapon: 'candlestick',
+});
+
+console.log(obj);
