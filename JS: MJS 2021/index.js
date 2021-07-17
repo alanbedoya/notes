e64681c133c803d7b12b452d6207d9e7a55be05b
Alan Bedoya <alanjbedoya@gmail.com>
2021-07-16 23:15:01 -0700

    Notes on Operator Precdence.

--- JS: MJS 2021/index.js
+++ JS: MJS 2021/index.js
@@ -1,1 +1,10 @@
 console.log('Hi');
+
+/*
+//Operator precdence
+
+ Operator precedence determines the way in which operators are paresed with respect to each other. Operators with higher precedence become the operands of operators with lower precedence. 
+
+
+
+*/
