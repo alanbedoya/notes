af926e23cc024de7f0f91dc1c241dfb11fffea19
Alan Bedoya <alanjbedoya@gmail.com>
2021-07-28 16:37:26 -0700

    Working on game logic.

--- JS: Notes 2021/numberGuessGame/index.js
+++ JS: Notes 2021/numberGuessGame/index.js
@@ -8,6 +8,11 @@
 
 // document.querySelector('.guess').value = 23;
 
+const randomNum = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = randomNum;
+
+let score = 20;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
@@ -15,5 +20,25 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number';
+  } else if (guess === randomNum) {
+    document.querySelector('.message').textContent = 'Corret Number!';
+  } else if (guess > randomNum) {
+    if (score > 2) {
+      document.querySelector('.message').textContent = 'Guess is too high!';
+      score -= 2;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game.';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < randomNum) {
+    if (score > 2) {
+      document.querySelector('.message').textContent = 'Guess is too low!';
+      score -= 2;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game.';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
