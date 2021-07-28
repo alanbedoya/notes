30b5e162aca9561826d7f074221030f14977590b
Alan Bedoya <alanjbedoya@gmail.com>
2021-07-28 01:27:14 -0700

    Working on game logic.

--- JS: Notes 2021/numberGuessGame/index.js
+++ JS: Notes 2021/numberGuessGame/index.js
@@ -12,4 +12,8 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
   console.log(typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number';
+  }
 });
