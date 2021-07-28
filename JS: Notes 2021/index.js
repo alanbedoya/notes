19144396384c9921a0c8d9ed84ead1ad7fe22917
Alan Bedoya <alanjbedoya@gmail.com>
2021-07-27 23:17:25 -0700

    Added to notes and to my js file.

--- JS: Notes 2021/index.js
+++ JS: Notes 2021/index.js
@@ -6,5 +6,8 @@ console.log('Notes');
  Operator precedence determines the way in which operators are paresed with respect to each other. Operators with higher precedence become the operands of operators with lower precedence. 
 
 
+ //What is the DOM?
+ //DOM
+ Document Object Model: Structured representation of HTML documents allows JS to access HTML elements and styles to manipulate them.
 
 */
