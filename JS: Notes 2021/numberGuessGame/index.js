c2942227b5257b0b6c646fee02c959e50919c723
Alan Bedoya <alanjbedoya@gmail.com>
2021-07-28 01:27:08 -0700

    Working on game logic.

--- JS: Notes 2021/numberGuessGame/index.js
+++ JS: Notes 2021/numberGuessGame/index.js
@@ -1,8 +1,15 @@
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number!';
-console.log(document.querySelector('.message').textContent);
+//Selecting and Manipulating Elements
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number!';
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
 
-document.querySelector('.guess').value = 23;
+// document.querySelector('.guess').value = 23;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  console.log(typeof guess);
+});
