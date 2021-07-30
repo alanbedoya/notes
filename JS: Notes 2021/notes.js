f36f22d3ed0dabbce8bae30674808a1aac2eb97c
Alan Bedoya <alanjbedoya@gmail.com>
2021-07-30 03:14:08 -0700

    Added a note.

--- JS: Notes 2021/notes.js
+++ JS: Notes 2021/notes.js
@@ -10,4 +10,7 @@ console.log('Notes');
  //DOM
  Document Object Model: Structured representation of HTML documents allows JS to access HTML elements and styles to manipulate them.
 
+
+ //classList
+ When wrting a classList remove or add make sure to not put a '.' in front of the class becuase you're not selecting the class you're calling it.
 */
