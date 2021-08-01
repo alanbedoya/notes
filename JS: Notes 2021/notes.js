36538f55d616901dfd134cf49d9d9eb23bc0c9d3
Alan Bedoya <alanjbedoya@gmail.com>
2021-07-31 23:53:13 -0700

    Adding some more notes.

--- JS: Notes 2021/notes.js
+++ JS: Notes 2021/notes.js
@@ -14,5 +14,11 @@ console.log('Notes');
  //classList
  When wrting a classList remove or add make sure to not put a '.' in front of the class becuase you're not selecting the class you're calling it.
 
+
+ //Operator
+ +=	Addition assignment	Adds the value on the right to the variable value on the left, then returns the new variable value	x += 4;	x = x + 4;
+-=	Subtraction assignment	Subtracts the value on the right from the variable value on the left, and returns the new variable value	x -= 3;	x = x - 3;
+*=	Multiplication assignment	Multiplies the variable value on the left by the value on the right, and returns the new variable value	x *= 3;	x = x * 3;
+/=	Division assignment	Divides the variable value on the left by the value on the right, and returns the new variable value	x /= 5;	x = x / 5;
  //
 */
