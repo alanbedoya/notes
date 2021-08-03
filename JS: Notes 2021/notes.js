75bb585d1d845f51aa7c851168a51e7292a452a1
Alan Bedoya <alanjbedoya@gmail.com>
2021-08-03 14:57:25 -0700

    Added more notes to notes.js about scopes in practice.

--- JS: Notes 2021/notes.js
+++ JS: Notes 2021/notes.js
@@ -137,6 +137,28 @@ console.log('Notes');
 
 
 //Scoping in Practice 
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Alan';
+calcAge(1993);
 
 
 
