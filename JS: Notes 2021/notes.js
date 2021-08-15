861e789fdfb42ff6f9fe4447fac3235ff0ecf6f9
Alan Bedoya <alanjbedoya@gmail.com>
2021-08-15 00:17:30 -0700

    Added notes on the rest pattern and parameters.

--- JS: Notes 2021/notes.js
+++ JS: Notes 2021/notes.js
@@ -407,7 +407,70 @@ calcAge(1993);
 
 
  // The Spread Operator (...) 
- * 
+ * EX: 
+    * const restaurant = {
+      name: 'Classico Italiano',
+      location: 'Via Angelo Tavanti 23, Firenze, Italy',
+      categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+      starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+      mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+      openingHours: {
+        thu: {
+          open: 12,
+          close: 22,
+        },
+        fri: {
+          open: 11,
+          close: 23,
+        },
+        sat: {
+          open: 0, // Open 24 hours
+          close: 24,
+        },
+      },
+    
+      order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+      },
+    
+      orderDelivery: function ({
+        starterIndex = 1,
+        mainIndex = 0,
+        time = '5:00pm',
+        address,
+      }) {
+        console.log(
+          `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}.`
+        );
+      },
+    };
+
+    const arr = [7, 8, 9];
+    const theBadWay = [1, 2, arr[0], arr[1], arr[2]];
+    console.log(theBadWay);
+
+    const theRightWay = [1, 2, ...arr];
+    console.log(theRightWay);
+
+    const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+    console.log(newMenu);
+
+  * Copy Array
+    * const mainMenuCopy = [...restaurant.mainMenu];
+
+  * Join 2 arrays
+    * const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+      console.log(menu);
+
+  * Iterables: arrays, strings, maps, sets. NOT objects
+    * const str = 'Alan';
+      const letters = [...str, ' ', 'B.'];
+      console.log(letters);
+
+ 
+// Rest Pattern and parameters
+* EX:      
 
 
 
@@ -491,26 +554,49 @@ const restaurant = {
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}.`
     );
   },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your declicious pasta with ${ing1}, ${ing2}, and ${ing3}`
+    );
+  },
+
+  orderPizza: function (mainIngredient, ...otherIngredient) {
+    console.log(mainIngredient);
+    console.log(otherIngredient);
+  },
 };
 
-const arr = [7, 8, 9];
-const theBadWay = [1, 2, arr[0], arr[1], arr[2]];
-console.log(theBadWay);
+//SPREAD, because on RIGHT side of =
+const arr = [1, 2, ...[3, 4]];
 
-const theRightWay = [1, 2, ...arr];
-console.log(theRightWay);
+//REST, because on LEFT side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
 
-const newMenu = [...restaurant.mainMenu, 'Gnocci'];
-console.log(newMenu);
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
 
-//Copy Array
-const mainMenuCopy = [...restaurant.mainMenu];
+//Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
 
-//Join 2 arrays
-const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
-console.log(menu);
+//Functions
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
 
-//Iterables: arrays, strings, maps, sets. NOT objects
-const str = 'Alan';
-const letters = [...str, ' ', 'B.'];
-console.log(letters);
+add(2, 3);
+add(5, 4, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');
