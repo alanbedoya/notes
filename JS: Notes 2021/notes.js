c0b0db9e8e2b943f1a7f042d87e337486425e581
Alan Bedoya <alanjbedoya@gmail.com>
2021-08-15 18:52:04 -0700

    Added notes on short circuiting (&& ||)

--- JS: Notes 2021/notes.js
+++ JS: Notes 2021/notes.js
@@ -470,10 +470,169 @@ calcAge(1993);
 
  
 // Rest Pattern and parameters
-* EX:      
+* EX: 
+  * const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+    openingHours: {
+      thu: {
+        open: 12,
+        close: 22,
+      },
+      fri: {
+        open: 11,
+        close: 23,
+      },
+      sat: {
+        open: 0, // Open 24 hours
+        close: 24,
+      },
+    },
+
+    order: function (starterIndex, mainIndex) {
+      return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+
+    orderDelivery: function ({
+      starterIndex = 1,
+      mainIndex = 0,
+      time = '5:00pm',
+      address,
+    }) {
+      console.log(
+        `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}.`
+      );
+    },
+
+    orderPasta: function (ing1, ing2, ing3) {
+      console.log(
+        `Here is your declicious pasta with ${ing1}, ${ing2}, and ${ing3}`
+      );
+    },
+
+    orderPizza: function (mainIngredient, ...otherIngredient) {
+      console.log(mainIngredient);
+      console.log(otherIngredient);
+    },
+  };
+
+  * SPREAD, because on RIGHT side of =
+    * const arr = [1, 2, ...[3, 4]];
+
+  * REST, because on LEFT side of =
+    * const [a, b, ...others] = [1, 2, 3, 4, 5];
+      console.log(a, b, others);
+
+  const [pizza, , risotto, ...otherFood] = [
+    ...restaurant.mainMenu,
+    ...restaurant.starterMenu,
+  ];
+  onsole.log(pizza, risotto, otherFood);
+
+  * Objects
+    * const { sat, ...weekdays } = restaurant.openingHours;
+      console.log(weekdays);
+
+  * Functions
+    * const add = function (...numbers) {
+      let sum = 0;
+      for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+      console.log(sum);
+    };
+
+    add(2, 3);
+    add(5, 4, 7, 2);
+    add(8, 2, 5, 3, 2, 1, 4);
+
+    const x = [23, 5, 7];
+    add(...x);
+
+    restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+    restaurant.orderPizza('mushrooms');     
 
 
+// Short Circuiting (&& and ||)
+* EX:
+  * const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+    openingHours: {
+      thu: {
+        open: 12,
+        close: 22,
+      },
+      fri: {
+        open: 11,
+        close: 23,
+      },
+      sat: {
+        open: 0, // Open 24 hours
+        close: 24,
+      },
+    },
+
+    order: function (starterIndex, mainIndex) {
+      return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+
+    orderDelivery: function ({
+      starterIndex = 1,
+      mainIndex = 0,
+      time = '5:00pm',
+      address,
+    }) {
+      console.log(
+        `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}.`
+      );
+    },
+
+    orderPasta: function (ing1, ing2, ing3) {
+      console.log(
+        `Here is your declicious pasta with ${ing1}, ${ing2}, and ${ing3}`
+      );
+    },
+
+    orderPizza: function (mainIngredient, ...otherIngredient) {
+      console.log(mainIngredient);
+      console.log(otherIngredient);
+    },
+  };
+
+  * Logical operators:
+    * Use ANY data type, return ANY data type, short-circut evalution or short-circuiting
+  * OR
+    * console.log(6 || 'Alan');
+      console.log('' || 'Alan');
+      console.log(true || 0);
+      console.log(undefined || null);
+
+      restaurant.numGuests = 23;
+      const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+      console.log(guests1);
+
+      const guests2 = restaurant.numGuests || 10;
+      console.log(guests2);
+
+  * AND
+    * console.log('--- AND ---');
+    console.log(0 && 'Alan');
+    console.log(6 && 'Alan');
+    console.log('Hello' && 23 && null && 'Alan');
+
+  * Practical Example
+    * if (restaurant.orderPizza) {
+        restaurant.orderPizza('pepperonie', 'mushrooms');
+      }
+
+      restaurant.orderPizza && restaurant.orderPizza('pepperonie', 'mushrooms');
 
 
 
@@ -567,36 +726,30 @@ const restaurant = {
   },
 };
 
-//SPREAD, because on RIGHT side of =
-const arr = [1, 2, ...[3, 4]];
+//Logical operators:
+//Use ANY data type, return ANY data type, short-circut evalution or short-circuiting
+//OR
+console.log(6 || 'Alan');
+console.log('' || 'Alan');
+console.log(true || 0);
+console.log(undefined || null);
 
-//REST, because on LEFT side of =
-const [a, b, ...others] = [1, 2, 3, 4, 5];
-console.log(a, b, others);
+restaurant.numGuests = 23;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
 
-const [pizza, , risotto, ...otherFood] = [
-  ...restaurant.mainMenu,
-  ...restaurant.starterMenu,
-];
-console.log(pizza, risotto, otherFood);
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
 
-//Objects
-const { sat, ...weekdays } = restaurant.openingHours;
-console.log(weekdays);
+//AND
+console.log('--- AND ---');
+console.log(0 && 'Alan');
+console.log(6 && 'Alan');
+console.log('Hello' && 23 && null && 'Alan');
 
-//Functions
-const add = function (...numbers) {
-  let sum = 0;
-  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
-  console.log(sum);
-};
+//Practical Example
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('pepperonie', 'mushrooms');
+}
 
-add(2, 3);
-add(5, 4, 7, 2);
-add(8, 2, 5, 3, 2, 1, 4);
-
-const x = [23, 5, 7];
-add(...x);
-
-restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
-restaurant.orderPizza('mushrooms');
+restaurant.orderPizza && restaurant.orderPizza('pepperonie', 'mushrooms');
